1b0729ab64543f5bb3c44f0f980ac8920e909713
paszed <edvard.pasz@icloud.com>
2026-04-02 00:55:56 +0200

    feat: add multi-field extraction via --item and --field

--- tractor/cli.py
+++ tractor/cli.py
@@ -1,21 +1,42 @@
 import argparse
 
 from tractor.fetch import fetch_html
-from tractor.extract import extract
+from tractor.extract import extract, extract_fields
 from tractor.output import output
 
 
 def run():
     parser = argparse.ArgumentParser(description="Tractor - simple web scraper")
 
-    parser.add_argument("url")
-    parser.add_argument("--selector", required=True)
-    parser.add_argument("--attr", default="text")
-    parser.add_argument("--format", default="text", choices=["text","json"])
+    parser.add_argument("url", help="URL to scrape")
+
+    # simple mode
+    parser.add_argument("--selector", help="CSS selector")
+    parser.add_argument("--attr", default="text", help="Attribute to extract")
+
+    # structured mode
+    parser.add_argument("--item", help="Selector for repeating items")
+    parser.add_argument(
+        "--field",
+        action="append",
+        help='Field mapping: name=selector or name=selector@attr'
+    )
+
+    # output
+    parser.add_argument("--format", default="text", choices=["text", "json"])
 
     args = parser.parse_args()
 
+    # validation
+    if not args.item and not args.selector:
+        parser.error("provide either --selector OR --item with --field")
+
     html = fetch_html(args.url)
-    data = extract(html, args.selector, args.attr)
+
+    # choose mode
+    if args.item and args.field:
+        data = extract_fields(html, args.item, args.field)
+    else:
+        data = extract(html, args.selector, args.attr)
 
     output(data, args.format)
--- tractor/extract.py
+++ tractor/extract.py
@@ -14,3 +14,38 @@ def extract(html, selector, attr="text"):
             results.append(el.get(attr))
 
     return results
+
+
+def extract_fields(html, item_selector, field_defs):
+    soup = BeautifulSoup(html, "html.parser")
+    items = soup.select(item_selector)
+
+    results = []
+
+    for item in items:
+        obj = {}
+
+        for field in field_defs:
+            # parse "name=selector@attr"
+            name, expr = field.split("=", 1)
+
+            if "@" in expr:
+                selector, attr = expr.split("@", 1)
+            else:
+                selector, attr = expr, "text"
+
+            el = item.select_one(selector)
+
+            if el:
+                if attr == "text":
+                    value = el.get_text(strip=True)
+                else:
+                    value = el.get(attr)
+            else:
+                value = None
+
+            obj[name] = value
+
+        results.append(obj)
+
+    return results
